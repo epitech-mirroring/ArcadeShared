669b0d91b4716715f193638fd19f985c7af0cff3
Antoinegtir <antoine.gonthier@epitech.eu>
2024-03-20 13:48:03 +0100

    :goal_net: Add IGameError

---
+++ errors/IGameError.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** ArcadeShared
+** File description:
+** No file there , just an epitech header example .
+** You can even have multiple lines if you want !
+*/
+
+#include "../IGame.hpp"
+#include "IError.hpp"
+
+class IGameError: public IError {
+public:
+    virtual ~IGameError() = default;
+    virtual int getCode() const = 0;
+    virtual std::shared_ptr<IGame> getGame() const = 0;
+    virtual const char *what() const noexcept = 0;
+};
