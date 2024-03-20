170d7845c2219a2b5fb94375360a29db41d09eb2
Antoinegtir <antoine.gonthier@epitech.eu>
2024-03-20 14:53:59 +0100

    :art: add default constant varibales (up, down..)

--- utils/ICanRotate.hpp
+++ utils/ICanRotate.hpp
@@ -9,6 +9,12 @@
 #pragma once
 
 class ICanRotate {
+
+const float UP = 90;
+const float DOWN = 270;
+const float LEFT = 180;
+const float RIGHT = 0;
+
 public:
     virtual ~ICanRotate() = default;
     virtual float getRotation() = 0;
