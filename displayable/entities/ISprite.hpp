8194e06fbf110ffd1647d49793201e2ba079fd44
RenardFute <axel.eckenberg@yahoo.fr>
2024-03-25 16:47:58 +0100

    :bug: Add missing destructor

--- displayable/entities/ISprite.hpp
+++ displayable/entities/ISprite.hpp
@@ -12,6 +12,7 @@
 
 class ISprite {
 public:
+    virtual ~ISprite() = default;
     virtual std::shared_ptr<IPicture> getPicture() = 0;
     virtual void setPicture(std::unique_ptr<IPicture> picture) = 0;
 };
