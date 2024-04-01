6287a2fc198818375b15d58b43318f6c2b3e334f
RenardFute <axel.eckenberg@yahoo.fr>
2024-04-01 20:56:11 +0200

    :sparkles: Add draw rect to sprites

--- displayable/entities/ISprite.hpp
+++ displayable/entities/ISprite.hpp
@@ -10,10 +10,19 @@
 #include <memory>
 #include "../../utils/IPicture.hpp"
 
+struct DrawRect {
+    std::size_t x;
+    std::size_t y;
+    std::size_t width;
+    std::size_t height;
+};
+
 class ISprite {
 public:
     virtual ~ISprite() = default;
     [[nodiscard]] virtual const IPicture &getPicture() const = 0;
     virtual void setPicture(const IPicture &picture) = 0;
     virtual void setPicture(std::unique_ptr<IPicture> picture) = 0;
+    [[nodiscard]] virtual const DrawRect &getDrawRect() const = 0;
+    virtual void setDrawRect(const DrawRect &drawRect) = 0;
 };
