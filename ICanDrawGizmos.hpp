34d039030a0e9a3219c7ed4fb1f067d61ec0c22b
RenardFute <axel.eckenberg@yahoo.fr>
2024-04-02 14:37:00 +0200

    :sparkles: Add new interfaces to draw gizmos

---
+++ ICanDrawGizmos.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** No file there , just an epitech header example .
+** You can even have multiple lines if you want !
+*/
+
+#pragma once
+#include "utils/ICoordinate.hpp"
+#include "utils/IColor.hpp"
+
+class ICanDrawGizmos {
+public:
+    virtual ~ICanDrawGizmos() = default;
+
+    virtual bool isGizmosEnabled() const = 0;
+    virtual void drawLine(const ICoordinate &start, const ICoordinate &end, const IColor &color) const = 0;
+    virtual void drawCircle(const ICoordinate &center, float radius, const IColor &color) const = 0;
+    virtual void drawRect(const ICoordinate &topLeft, const ICoordinate &bottomRight, bool fill, const IColor &color) const = 0;
+};
