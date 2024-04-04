d203e949cd8d3dfc429319b0ccb45fdd63f6dd37
Antoinegtir <antoine.gonthier@epitech.eu>
2024-04-04 17:45:34 +0200

    :loud_sound: add sound interface

---
+++ sound/ISound.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** ArcadeShared
+** File description:
+** No file there , just an epitech header example .
+** You can even have multiple lines if you want !
+*/
+
+#pragma once
+
+#include "../utils/ICoordinate.hpp"
+#include <string>
+
+class ISound {
+public:
+    virtual ~ISound() = default;
+    [[nodiscard]] virtual const std::string &getPath() const = 0;
+    virtual void setPath(const std::string &path) = 0;
+    [[nodiscard]] virtual void setRepeat(bool isRepeating) = 0;
+    virtual bool getRepeat() = 0;
+};
